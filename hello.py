9ed291eb68781a039f963a316a5c7923434bdf7d
iMaary <m.rodriguesnave@gmail.com>
2021-05-08 14:16:44 -0300

    empty route created

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@ from flask import Flask, render_template
 
 # now create a flask instance
 app = Flask(__name__)
+
+#create a route decorate (it's how ...url.../home.html)
+@app.route('/') # it works when there ins't route name
+
+def index():
+    return "<h1>Hello World!</h1>"
