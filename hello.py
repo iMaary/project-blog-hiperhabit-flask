dd6e8c2f173cf851ae231260a614325a50cf43a6
iMaary <m.rodriguesnave@gmail.com>
2021-05-08 14:55:38 -0300

    calling html of index webpage with render_template

--- hello.py
+++ hello.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 #create a route decorate (it's how ...url.../home.html)
 @app.route('/') # it works when there ins't route name
 
+#def index():
+#    return "<h1>Hello World!</h1>"
+
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
 
 # localhost:5000/user/mary
 @app.route('/user/<name>')
