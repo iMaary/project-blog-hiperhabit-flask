95f3aaf72c95940e6fa53925b7141a2f8ea0a8de
iMaary <m.rodriguesnave@gmail.com>
2021-05-10 16:48:55 -0300

    simple form that ask name and return this in html

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # now create a flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "senha muito secreta"
+
+# Create a form class
+class NamerForm(FlaskForm):
+    name = StringField("Qual é o seu nome?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 #create a route decorate (it's how ...url.../home.html)
 @app.route('/') # it works when there ins't route name
@@ -46,3 +55,17 @@ def page_not_fount(e):
 @app.errorhandler(500)
 def page_not_fount(e):
     return render_template("500.html")
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",
+                            name = name,
+                            form = form)
