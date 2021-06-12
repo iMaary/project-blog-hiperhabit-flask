e71170cdbb3f12da9b5c4c2ef27851973d7e402d
iMaary <m.rodriguesnave@gmail.com>
2021-06-12 15:58:00 -0300

    using hashed password to registration

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, flash, request
 from flask.signals import request_finished
 from flask_wtf import FlaskForm, form
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.fields.simple import PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 from datetime import datetime
@@ -75,6 +76,8 @@ class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_color = StringField("Favorite Color")
+    password_hash = PasswordField("Senha", validators=[DataRequired(), EqualTo('password_hash2', message='As senhas devem combinar')])
+    password_hash2 = PasswordField("Confirmar Senha", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 # Update Database Record
@@ -151,13 +154,16 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
+            # Hash the password!
+            hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data, password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
         form.favorite_color.data = ''
+        form.password_hash.data  = ''
         flash("Usuário Adicionado Com Sucesso!")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html", name=name,
