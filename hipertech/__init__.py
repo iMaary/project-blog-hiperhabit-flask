e31270b4a611d3f7af4a28f08ce0353969520a12
iMaary <m.rodriguesnave@gmail.com>
2021-06-13 18:40:58 -0300

    mysql problem fixed

--- hipertech/__init__.py
+++ hipertech/__init__.py
@@ -5,10 +5,11 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:62777999&aooa@localhost/our_users'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = "senha muito secreta"
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 import hipertech.models
 import hipertech.routes
